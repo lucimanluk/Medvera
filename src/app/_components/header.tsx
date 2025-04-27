62e188441dd672aaca51e7e8d9a69518f616a477
lucimanluk <lucimanluk4000@gmail.com>
2025-04-27 22:32:04 +0300

    Created the navbar for the landing page.

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,43 @@
+import { Button } from "~/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+  const hrefs = [
+    "Features",
+    "How it works",
+    "Specialities",
+    "Testimonials",
+    "Pricing",
+    "FAQ",
+  ];
+
+  return (
+    <nav className="fixed top-0 left-0 z-50 flex w-full items-center justify-between bg-white p-4 shadow-sm">
+      <div className="flex items-center gap-2">
+        <Image src="/favicon.ico" alt="Medvera logo" width={32} height={32} />
+        <span className="text-xl font-bold text-black">Medvera</span>
+      </div>
+      <ul className="flex items-center gap-6 text-sm font-medium">
+        {hrefs.map((href, index) => (
+          <li key={index}>
+            <Link
+              href={`#${href.toLowerCase().replace(/\s+/g, "-")}`}
+              className="text-md text-black transition-colors hover:text-blue-600"
+            >
+              {href}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center gap-4">
+        <Button variant="ghost" className="rounded-3xl">
+          Login
+        </Button>
+        <Button className="rounded-3xl bg-[#2F80ED] text-white hover:bg-[#1366d6]">
+          Sign Up
+        </Button>
+      </div>
+    </nav>
+  );
+}
