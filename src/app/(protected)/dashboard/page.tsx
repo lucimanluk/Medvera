3c98773299a10d4954305e363d2b26f9d776e027
LucianTrifan <luciant@admintools.io>
2025-06-03 13:19:01 +0300

    Created the containers with mock data for incomming appointments and messages in the dashboard.

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,51 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { ScrollArea, ScrollBar } from "~/components/ui/scroll-area";
+import Appointment from "../_components/appointment";
+
+const appointments = [{
+  doctor_name: "Andrei Denis",
+  doctor_speciality: "Cardiology",
+  date: "10/06/2025",
+  time: "21:15",
+  appointment_type: "virtual",
+  location: "",
+  avatar: "",
+},
+{
+  doctor_name: "Andrei Denis",
+  doctor_speciality: "Cardiology",
+  date: "10/06/2025",
+  time: "21:15",
+  appointment_type: "virtual",
+  location: "",
+  avatar: "",
+},
+{
+  doctor_name: "Andrei Denis",
+  doctor_speciality: "Cardiology",
+  date: "10/06/2025",
+  time: "21:15",
+  appointment_type: "virtual",
+  location: "",
+  avatar: "",
+},
+{
+  doctor_name: "Andrei Denis",
+  doctor_speciality: "Cardiology",
+  date: "10/06/2025",
+  time: "21:15",
+  appointment_type: "virtual",
+  location: "",
+  avatar: "",
+},
+];
+
 export default function Dashboard() {
   return (
     <div className="flex w-full flex-col gap-4 py-4 pr-4">
@@ -6,6 +54,26 @@ export default function Dashboard() {
         <span className="text-base text-gray-400">
           Quick view into your prescriptions, appointments and messages
         </span>
+      </div>
+      <div className="flex flex-row justify-between w-full gap-4">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Appointments</CardTitle>
+            <CardDescription>See your future appointments</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {appointments.length === 0 ? <span>No upcoming appointments.</span> : <ScrollArea className="h-72 w-full">{appointments.map((appointment, index) => <div key = {index} className="p-2"><Appointment/></div>)}</ScrollArea>}
+          </CardContent>
+        </Card>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Messages</CardTitle>
+            <CardDescription>See what messages you have received</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {appointments.length === 0 ? <span>No upcoming appointments.</span> : <ScrollArea className="h-72 w-full">{appointments.map((appointment, index) => <div key = {index} className="p-2"><Appointment/></div>)}</ScrollArea>}
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
